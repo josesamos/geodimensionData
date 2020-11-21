eb3a47d647da14fb59c87a78d498bff7c81f6c9d
Jose Samos <jsamos@ugr.es>
2020-11-21 20:31:20 +0100

    New data set.

---
+++ R/data.R
@@ -0,0 +1,13 @@
+
+#' `gd_es_municipio`
+#'
+#' `geodimension` obtained from vector layers over Spain.
+#'
+#' It includes the levels municipality, livestock region, agricultural region,
+#' province, autonomous community and country.
+#'
+#' @format A `geodimension`.
+#' @source
+#'   \url{https://www.ign.es}
+"gd_es_municipio"
+
